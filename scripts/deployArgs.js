09fa6b6cabc96ce4ac6f7fcf0fd68b690baa7646
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-07-27 17:28:03 -0500

    Update deployArgs.js

--- scripts/deployArgs.js
+++ scripts/deployArgs.js
@@ -1,26 +1,24 @@
 module.exports = [
- 'Farm Folk NFT',
- 'FFNFT',
+ 'Passive Inu Token',
+ 'PINU',
  
-    '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
-    '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
-    'https://public-pre-ipfs.s3.amazonaws.com/Farm_Folk_NFT/json/',
-    'https://public-pre-ipfs.s3.amazonaws.com/Farm_Folk_NFT/assets/FarmFolkReveal.json',
+ '0x35b277fe28843881D9477869E6E229CD04E71aB2',
+ '0x0B5CEea457567EcEF97Cb744E585bdAC5FEDb42e',
+ 'https://public-pre-ipfs.s3.amazonaws.com/Pass_NFT/json/',
+ 'https://ipfs.s3.amazonaws.com/Passive_Inu/json/',
 
-    [
-        ethers.utils.getAddress('0xC3ea7494c33AbCA4FcEa01331043311756b7e336'),
-        ethers.utils.getAddress('0x8912e40d291693C7A86C418D52b46766780C6989'),
-        ethers.utils.getAddress('0x6b3ba9F0475C646FDaF2Bc207C285a485B88A747'),
-        ethers.utils.getAddress('0xbE400D9A6f8140B51dE4B3b7AAb1DA4B9adec9a0'),
-        ethers.utils.getAddress('0xEAC3ce292F95d779732e7a26c95c57A742cf5119'),
-        ethers.utils.getAddress('0x9Dc7D54E10A199C00176FFA68459fD9b15A706c4')
-    ],
-    [
-        26,
-        12,
-        10,
-        6,
-        5,
-        41
-    ]
+ [
+     ethers.utils.getAddress('0x0B5CEea457567EcEF97Cb744E585bdAC5FEDb42e'),
+     ethers.utils.getAddress('0x8f25a6b9c1fb03355333b80f78288a666a293a91'),
+     ethers.utils.getAddress('0x629F5E384037cA37A563C9a17B98055f937f208d'),
+     ethers.utils.getAddress('0xc2689Ddf02E4b77BdB7ae6F5EC890FF5964e09B3'),
+     ethers.utils.getAddress('0x83458be9FE4a42b0A0D79BED404d97ed1c4a9937')
+ ],
+ [
+     20,
+     20,
+     20,
+     20,
+     20
+ ]
 ]
