0a9f7c897e1860df8283f0c8bcf9bf910aefb08d
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-07-29 12:31:24 -0500

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -61,15 +61,15 @@ module.exports = {
   //   }
   // }
 
-  // defaultNetwork: "mainnet",
-  // etherscan: {
-  //   apiKey: ETHERSCAN_API_KEY,
-  // },
-  // networks: {
-  //   hardhat: { chainId: 25 },
-  //   mainnet: {
-  //     url: `https://evm-cronos.crypto.org/`,
-  //     accounts: [`0x${PK}`],
-  //   }
-  // }  
+  defaultNetwork: "mainnet",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  networks: {
+    hardhat: { chainId: 25 },
+    mainnet: {
+      url: `https://evm-cronos.crypto.org/`,
+      accounts: [`0x${PK}`],
+    }
+  }  
 };
